bf41adadc577b259d6c0bf6352ebecec411288d4
Binghuixie <1479085300@qq.com>
2021-01-30 01:41:55 +0800

    add: 美国疫情地图（没有阿拉斯加）

--- src/pages/firstPage/index.tsx
+++ src/pages/firstPage/index.tsx
@@ -5,7 +5,14 @@ import './style.scss';
 export default class FirstPage extends Vue {
     public render() {
         return (
-            <div class='fullpage-container__section'>首屏</div>
+            <div class='fullpage-container__section first-page'>
+                <div class='video-container'>
+                    <video
+                        controls
+                        src='https://lark-video.oss-cn-hangzhou.aliyuncs.com/outputs/prod/yuque/2021/1226124/mp4/1610285747349-7e8637c2-66e6-419f-a660-00360fbe10e5.mp4?OSSAccessKeyId=LTAI4GGhPJmQ4HWCmhDAn4F5&Expires=1611919585&Signature=TSKNZOIiG9Uz0GtfJLNA72qkGPo%3D'
+                    ></video>
+                </div>
+            </div>
         );
     }
 }
--- src/pages/usaEpidemic/index.tsx
+++ src/pages/usaEpidemic/index.tsx
@@ -1,11 +1,207 @@
 import { Component, Vue } from 'vue-property-decorator';
+import DataSet from '@antv/data-set';
+import { Chart, View } from '@antv/g2';
+import USAJson from '@/mock/usa.json';
+import USAStateJson from '@/mock/foreign-list.json';
+import { IUSAStateEpidemic } from '@/interfaces';
+import { findMaxComfirmFromGeoData, findMinComfirmFromGeoData } from '@/utlis/array.utli';
 import './style.scss';
 
 @Component({})
 export default class UsaEpidemic extends Vue {
+
+    public mapData!: any;
+
+    public epidemicData!: IUSAStateEpidemic[];
+
+    public chart!: View;
+
+    public dataset!: any;
+
+    public $refs!: {
+        usaMap: HTMLDivElement,
+    };
+
+    public scaleState(source: any, name: string, latStep: number, longStep: number) {
+        const state = source.rows.find((row: any) => row.name === name);
+        const stateIndex = source.rows.findIndex((row: any) => row.name === name);
+        const { longitude, latitude } = state;
+        const scaleLatitude = latitude.map((coord: number) => {
+            return coord + latStep;
+        });
+        const scaleLongitude = longitude.map((coord: number) => {
+            return coord + longStep;
+        });
+        state.latitude = scaleLatitude;
+        state.longitude = scaleLongitude;
+        source.rows.splice(stateIndex, 1, state);
+    }
+
+    public setTooltip() {
+        if (this.chart) {
+            this.chart.tooltip({
+                showTitle: false,
+            });
+        }
+    }
+
+    public setScale() {
+        if (this.chart) {
+            this.chart.scale({
+                longitude: {
+                  sync: true,
+                },
+                latitude: {
+                  sync: true,
+                },
+              });
+        }
+    }
+
+    public setAxis(axis: boolean) {
+        if (this.chart) {
+            this.chart.axis(axis);
+        }
+    }
+
+    public setLegend(legend: boolean | string, config?: any) {
+        if (this.chart) {
+            if (typeof legend === 'boolean') {
+                this.chart.legend(legend);
+            } else {
+                this.chart.legend(legend, config);
+            }
+        }
+    }
+
+    /**
+     * 美国地图背景
+     */
+    public drawDataSetBackground(maxState: IUSAStateEpidemic, minState: IUSAStateEpidemic) {
+        this.dataset = new DataSet();
+        const usaMap = this.dataset.createView('back')
+            .source(this.mapData, {
+                type: 'GeoJSON',
+            });
+        this.scaleState(usaMap, 'Hawaii', 8, 51);
+        this.scaleState(usaMap, 'Puerto Rico', 10, -25);
+        const worldMapView = this.chart.createView();
+        worldMapView.data(usaMap.rows);
+        worldMapView.tooltip(false);
+        worldMapView.polygon().position('longitude*latitude').style({
+            fill: '#fff',
+            stroke: '#ccc',
+            lineWidth: 1,
+        });
+
+        this.drawDataSetUserView(usaMap, {
+            maxConfirm: maxState.confirm,
+            minConfirm: minState.confirm,
+        });
+    }
+
+    /**
+     * 用户可视化数据
+     */
+    public drawDataSetUserView(usaMap: any, range: { maxConfirm: number, minConfirm: number }) {
+        const { maxConfirm, minConfirm } = range;
+        const baseColor = {
+            r: 255,
+            g: 0,
+            b: 0,
+        };
+        const endColor = {
+            r: 70,
+            g: 0,
+            b: 0,
+        };
+        const colorStep = {
+            r: Math.abs((baseColor.r - endColor.r)) / (maxConfirm - minConfirm),
+            g: Math.abs((baseColor.g - endColor.g)) / (maxConfirm - minConfirm),
+            b: Math.abs((baseColor.b - endColor.b)) / (maxConfirm - minConfirm),
+        };
+        const userDv = this.dataset.createView()
+            .source(this.epidemicData)
+            .transform({
+                geoDataView: usaMap,
+                field: 'nameMap',
+                type: 'geo.region',
+                as: ['longitude', 'latitude'],
+            });
+        const userView = this.chart.createView();
+        userView.data(userDv.rows);
+        userView.scale({
+            name: {
+                alias: '州',
+            },
+            nameMap: {
+                alias: 'State',
+            },
+            confirm: {
+                alias: '确诊人数',
+            },
+        });
+        userView.polygon()
+            .position('longitude*latitude')
+            .color('confirm', (confirm: number) => {
+                const steps = confirm - minConfirm;
+                const r = Math.floor(baseColor.r - steps * colorStep.r);
+                const g = Math.floor(baseColor.g - steps * colorStep.g);
+                const b = Math.floor(baseColor.b - steps * colorStep.b);
+                return `rgba(${r}, ${g}, ${b})`;
+            })
+            .tooltip('nameMap*confirm*name')
+            .style({
+                fillOpacity: 0.85,
+            })
+            .animate({
+                leave: {
+                    animation: 'fade-out',
+                },
+            });
+
+        userView.interaction('element-active');
+    }
+
+    public renderChart() {
+        this.chart.render();
+    }
+
+    public mounted() {
+        this.mapData = USAJson;
+        this.epidemicData = USAStateJson.data.children;
+        this.chart = new Chart({
+            container: 'usa-map__container',
+            autoFit: true,
+            height: 450,
+            width: window.innerWidth / 2,
+            padding: [20, 18],
+        });
+
+        const maxConfirmState = findMaxComfirmFromGeoData(this.epidemicData);
+        const minConfirmState = findMinComfirmFromGeoData(this.epidemicData);
+
+        this.setTooltip();
+
+        this.setScale();
+
+        this.setLegend(false);
+
+        this.setAxis(false);
+
+        this.drawDataSetBackground(maxConfirmState, minConfirmState);
+
+        this.renderChart();
+    }
+
     public render() {
         return (
-            <div class='fullpage-container__section'>美国疫情板块</div>
+            <div class='fullpage-container__section usa-epidemic'>
+                <div
+                    id='usa-map__container'
+                    ref='usaMap'
+                ></div>
+            </div>
         );
     }
 }
