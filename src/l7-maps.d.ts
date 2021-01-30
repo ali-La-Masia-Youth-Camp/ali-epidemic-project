233d582af635fcbb754c6b6432b5142af6a91838
JackYu <779618724@qq.com>
2021-01-30 19:23:48 +0800

    add:足坛地图初始化

---
+++ src/l7-maps.d.ts
@@ -0,0 +1,9 @@
+/*
+ * @Author: your name
+ * @Date: 2021-01-30 17:25:37
+ * @LastEditTime: 2021-01-30 17:25:58
+ * @LastEditors: your name
+ * @Description: In User Settings Edit
+ * @FilePath: \ali-epidemic-project\src\l7-maps.d.ts
+ */
+declare module '@antv/l7-maps'
--- src/pages/soccerEpidemic/index.tsx
+++ src/pages/soccerEpidemic/index.tsx
@@ -1,11 +1,25 @@
 import { Component, Vue } from 'vue-property-decorator';
+import { Scene, PolygonLayer, LineLayer, Popup } from '@antv/l7';
+import { GaodeMap } from '@antv/l7-maps';
 import './style.scss';
 
 @Component({})
 export default class SoccerEpidemic extends Vue {
     public render() {
         return (
-            <div class='fullpage-container__section'>足坛疫情板块</div>
+            <div class='fullpage-container__section'>
+                <div id="soccer-container" ></div>
+            </div>
         );
     }
 }
+
+const scene = new Scene({
+    id: 'soccer-container',
+    map: new GaodeMap({
+        style: 'dark',
+        center: [10.770672, 45.159869],
+        zoom: 5.5,
+        pitch: 0,
+    }),
+});
