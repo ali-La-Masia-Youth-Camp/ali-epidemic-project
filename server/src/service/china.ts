57d4d53aa9375cd978a9b33c2f8410ef165c4171
helu0220 <1456444576@qq.com>
2021-01-30 20:39:01 +0800

    'add:查询最近一个月感染人数最多的10个城市;查询最近一个月每天的确诊数量的后端接口'

--- server/src/service/china.ts
+++ server/src/service/china.ts
@@ -25,7 +25,7 @@ export class ChinaService {
 
   //查询每个省近一个月的新增病例
   async getProvince() {
-       const staticUrl='https://api.inews.qq.com/newsqa/v1/query/pubished/daily/list?province=';
+      const staticUrl='https://api.inews.qq.com/newsqa/v1/query/pubished/daily/list?province=';
       let data=[];
       await Promise.all(ChinaService.provinceNames.map( async province=>{
           let url=staticUrl+province;
@@ -62,4 +62,74 @@ export class ChinaService {
   async getNDayData(){
 
   }
+
+  //查询最近一个月每天的确证数量
+  async getDayCount(){
+    let result={
+      isOk:false,
+      data:[],
+      error:''
+    }
+    const data=await this.app.curl('https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=chinaDayList',{
+      method: 'GET',
+      dataType: 'json',
+    });
+    if(data!=null){
+      let count=0;
+      data.data.data.chinaDayList.reverse().some(item=>{
+        result.data.push({
+          date:item.date+' '+item.y,
+          value:item.nowConfirm
+        })
+        count++;
+        if(count==30){
+          return true;
+        }
+      })
+    }else{
+      result.error='获取疫情数据失败'
+    }
+    return result;
+  }
+
+  //获取近期确诊数目最多的十个城市
+  async getCityData(){
+    let result={
+      isOk:false,
+      data:[],
+      error:''
+    }
+    const data=await this.app.curl('https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=statisGradeCityDetail',{
+      method: 'GET',
+      dataType: 'json',
+    });
+    if(data!=null){
+      //对数据按照确诊数据排序
+      let sumData=data.data.data.statisGradeCityDetail;
+      if(sumData.length<=10){
+        result.isOk=true;
+        result.data=sumData;
+      }else{
+        for(let i=10;i<sumData.length;i++){
+            let min=sumData[0];
+            let d=0;
+            for(let j=1;j<10;j++){
+              if(sumData[j]<min){
+                min=sumData[j];
+                d=j;
+              }
+            }
+            if(sumData[i]>min){
+              sumData[d]=sumData[i];
+            }
+        }
+        result.isOk=true;
+        result.data=sumData.splice(10)
+      }
+    }else{
+      result.error='获取疫情数据失败'
+    }
+    return result;
+  }
+
 }
