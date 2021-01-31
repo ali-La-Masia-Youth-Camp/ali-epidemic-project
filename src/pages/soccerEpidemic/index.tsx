b3fd9db133f35e689ab46ff59a4628c192cd3ef0
JackYu <779618724@qq.com>
2021-01-31 17:30:18 +0800

    add:添加3d柱状图

--- src/pages/soccerEpidemic/index.tsx
+++ src/pages/soccerEpidemic/index.tsx
@@ -1,77 +1,37 @@
 /*
  * @Author: your name
  * @Date: 2021-01-29 18:13:19
- * @LastEditTime: 2021-01-31 15:09:45
+ * @LastEditTime: 2021-01-31 17:29:45
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ali-epidemic-project\src\pages\soccerEpidemic\index.tsx
  */
 import { Component, Vue } from 'vue-property-decorator';
-import { Scene, Marker } from '@antv/l7';
+import { Scene, Marker, PointLayer, Popup } from '@antv/l7';
 import { GaodeMap } from '@antv/l7-maps';
+import leagueList from './mock/leagueList.json'
 import './style.scss';
 
 @Component({})
 export default class SoccerEpidemic extends Vue {
+
+    public leagueListData!: any;
 
     mounted() {
         const scene = new Scene({
             id: 'soccer-container',
             map: new GaodeMap({
                 style: 'dark',
-                center: [10.770672, 45.159869],
+                center: [10.770672, 46.159869],
                 zoom: 5.5,
-                maxZoom: 5.5,
                 minZoom: 5.5,
-                pitch: 0,
+                rotation: -0.76,
+                pitch: 48.62562,
                 token: 'ed39cda10e7496acd354632b9945313f'
             }),
         });
         scene.on('loaded', () => {
-            // 添加法甲logo
-            const el = document.createElement('img');
-            el.className = 'fj';
-            el.src = 'fj.png';
-            el.width = 75;
-            el.height = 44;
-            const marker = new Marker({ element: el }).setLnglat({ lng: 2.201204, lat: 48.513902 });
-            scene.addMarker(marker);
-
-            // 添加意甲logo
-            const el1 = document.createElement('img');
-            el1.className = 'yj';
-            el1.src = 'yj.png';
-            el1.width = 60;
-            el1.height = 65;
-            const marker1 = new Marker({ element: el1 }).setLnglat({ lng: 12.501204, lat: 41.813902 });
-            scene.addMarker(marker1);
-
-            // 添加西甲logo
-            const el2 = document.createElement('img');
-            el2.className = 'xj';
-            el2.src = 'xj.png';
-            el2.width = 75;
-            el2.height = 44;
-            const marker2 = new Marker({ element: el2 }).setLnglat({ lng: -3.421204, lat: 40.253902 });
-            scene.addMarker(marker2);
-
-            // 添加德甲logo
-            const el3 = document.createElement('img');
-            el3.className = 'dj';
-            el3.src = 'dj.png';
-            el3.width = 75;
-            el3.height = 44;
-            const marker3 = new Marker({ element: el3 }).setLnglat({ lng: 12.251204, lat: 52.303902 });
-            scene.addMarker(marker3);
-
-            // 添加英超logo
-            const el4 = document.createElement('img');
-            el4.className = 'yc';
-            el4.src = 'yc.png';
-            el4.width = 75;
-            el4.height = 44;
-            const marker4 = new Marker({ element: el4 }).setLnglat({ lng: 0.151204, lat: 51.303902 });
-            scene.addMarker(marker4);
+            this.leagueListData = leagueList.data;
 
             // 添加欧足联标志
             const el5 = document.createElement('img');
@@ -79,10 +39,79 @@ export default class SoccerEpidemic extends Vue {
             el5.src = 'uefa.png';
             el5.width = 120;
             el5.height = 120;
-            const marker5 = new Marker({ element: el5 }).setLnglat({ lng: -15.5852328, lat: 34.864434 });
+            const marker5 = new Marker({ element: el5 }).setLnglat({ lng: -7.5852328, lat: 36.864434 });
             scene.addMarker(marker5);
 
+            this.leagueListData.map((item: any) => {
+                let el = document.createElement('img');
+                el.className = item.league_Name;
+                el.src = item.league_Name + '.png';
+                el.width = 75;
+                el.height = 44;
+                if (item.league_Name === 'yj') {
+                    el.width = 60;
+                    el.height = 65;
+                }
+                let marker = new Marker({ element: el }).setLnglat({ lng: item.league_lng, lat: item.league_lat });
+                scene.addMarker(marker);
+            })
 
+            const pointLayer = new PointLayer({}).source(this.leagueListData, {
+                parser: {
+                    type: 'json',
+                    x: 'league_lng',
+                    y: 'league_lat'
+                }
+            }).shape('cylinder')
+                .size('confirm_all', function (level) {
+                    return [6, 6, level * 2 + 20];
+                })
+                .active(true)
+                .color('#E83132')
+                .style({
+                    opacity: 1.5,
+                    strokeWidth: 50
+                });
+
+            pointLayer.on('mousemove', e => {
+                const popup = new Popup({
+                    offsets: [0, 10],
+                    closeButton: false
+                })
+                    .setLnglat({ lng: e.feature.league_lng, lat: e.feature.league_lat })
+                    .setHTML(`<span>累计确诊: ${e.feature.confirm_all}</span>`);
+                scene.addPopup(popup);
+            });
+            scene.addLayer(pointLayer);
+
+
+            const pointLayer1 = new PointLayer({}).source(this.leagueListData, {
+                parser: {
+                    type: 'json',
+                    x: 'league_lng',
+                    y: 'league_lat'
+                }
+            }).shape('hexagonColumn')
+                .size('confirm_add', function (level) {
+                    return [6, 6, level * 5 + 20];
+                })
+                .active(true)
+                .color('#E83132')
+                .style({
+                    strokeWidth: 50,
+                    offsets: [500, 500]
+                });
+
+            pointLayer1.on('mousemove', e => {
+                const popup = new Popup({
+                    offsets: [0, 0],
+                    closeButton: false
+                })
+                    .setLnglat({ lng: e.feature.league_lng, lat: e.feature.league_lat })
+                    .setHTML(`<span>新增确诊: ${e.feature.confirm_add}</span>`);
+                scene.addPopup(popup);
+            });
+            scene.addLayer(pointLayer1);
             scene.render();
         });
 
