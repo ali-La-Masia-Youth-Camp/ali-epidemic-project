dc15e19ee67a6e435e32dc0e948661a80364519a
JackYu <779618724@qq.com>
2021-02-03 18:54:15 +0800

    add:添加治愈和新增的柱状图

--- src/pages/soccerEpidemic/index.tsx
+++ src/pages/soccerEpidemic/index.tsx
@@ -1,18 +1,19 @@
 /*
  * @Author: your name
  * @Date: 2021-01-29 18:13:19
- * @LastEditTime: 2021-02-01 00:15:19
+ * @LastEditTime: 2021-02-03 18:52:28
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \ali-epidemic-project\src\pages\soccerEpidemic\index.tsx
  */
 import { Component, Vue } from 'vue-property-decorator';
-import { Scene, Marker, PointLayer, Popup } from '@antv/l7';
+import { Scene, Marker, PointLayer, Popup, Scale, Zoom, Layers } from '@antv/l7';
 import { GaodeMap } from '@antv/l7-maps';
 import leagueList from './mock/leagueList.json';
 import Table from './component/table.vue';
 import Trend from './component/trend.vue';
 import './style.scss';
+import { Button } from 'element-ui';
 
 @Component({
     components: {
@@ -23,8 +24,15 @@ import './style.scss';
 export default class SoccerEpidemic extends Vue {
 
     public leagueListData!: any;
+    public leagueListData_ca!: any;
+    public leagueListData_ch!:any;
+
 
     public mounted() {
+
+
+
+
         const scene = new Scene({
             id: 'soccer-container',
             map: new GaodeMap({
@@ -39,6 +47,16 @@ export default class SoccerEpidemic extends Vue {
         });
         scene.on('loaded', () => {
             this.leagueListData = leagueList.data;
+            //每日新增柱状图的经纬度信息
+            this.leagueListData_ca = this.leagueListData.map((item: any) => ({
+                ...item,
+                league_lng: parseFloat(item.league_lng)+0.5+''
+            }));
+             //每日新增柱状图的经纬度信息
+             this.leagueListData_ch = this.leagueListData_ca.map((item: any) => ({
+                ...item,
+                league_lng: parseFloat(item.league_lng)+0.5+''
+            }));
 
             // 添加欧足联标志
             const el5 = document.createElement('img');
@@ -46,7 +64,7 @@ export default class SoccerEpidemic extends Vue {
             el5.src = 'uefa.png';
             el5.width = 120;
             el5.height = 120;
-            el5.onclick = function() {
+            el5.onclick = function () {
                 window.open('https://soccer.hupu.com/uefa/');
             };
             const marker5 = new Marker({ element: el5 }).setLnglat({ lng: -7.5852328, lat: 36.864434 });
@@ -56,11 +74,11 @@ export default class SoccerEpidemic extends Vue {
                 const el = document.createElement('img');
                 el.className = item.league_Name;
                 el.src = item.league_Name + '.png';
-                el.width = 75;
-                el.height = 44;
+                el.width = 80;
+                el.height = 75;
                 if (item.league_Name === 'yj') {
-                    el.width = 60;
-                    el.height = 65;
+                    el.width = 70;
+                    el.height = 85;
                 }
                 let href = '';
                 switch (item.league_Name) {
@@ -79,25 +97,26 @@ export default class SoccerEpidemic extends Vue {
                     default:
                         href = 'uefa';
                 }
-                el.onclick = function() {
+                el.onclick = function () {
                     window.open('https://soccer.hupu.com/' + href);
                 };
-                const marker = new Marker({ element: el }).setLnglat({ lng: item.league_lng, lat: item.league_lat });
+                const marker = new Marker({ element: el }).setLnglat({ lng:parseFloat(item.league_lng)-2.0, lat: item.league_lat });
                 scene.addMarker(marker);
             });
 
-            const pointLayer = new PointLayer({}).source(this.leagueListData, {
+            // 添加累计确诊的3d柱状图层
+            const pointLayer = new PointLayer({zIndex:999}).source(this.leagueListData, {
                 parser: {
                     type: 'json',
                     x: 'league_lng',
                     y: 'league_lat',
                 },
             }).shape('cylinder')
-                .size('confirm_all', function(level) {
+                .size('confirm_all', function (level) {
                     return [6, 6, level * 2 + 20];
                 })
                 .active(true)
-                .color('#E83132')
+                .color('blue')
                 .style({
                     opacity: 1.5,
                     strokeWidth: 50,
@@ -105,7 +124,7 @@ export default class SoccerEpidemic extends Vue {
 
             pointLayer.on('mousemove', (e) => {
                 const popup = new Popup({
-                    offsets: [0, 10],
+                    offsets: [0, 0],
                     closeButton: false,
                 })
                     .setLnglat({ lng: e.feature.league_lng, lat: e.feature.league_lat })
@@ -113,6 +132,78 @@ export default class SoccerEpidemic extends Vue {
                 scene.addPopup(popup);
             });
             scene.addLayer(pointLayer);
+
+            // 添加每日新增的3d柱状图层
+            const pointLayer1 = new PointLayer({zIndex:999}).source(this.leagueListData_ca, {
+                parser: {
+                    type: 'json',
+                    x: 'league_lng',
+                    y: 'league_lat',
+                },
+            }).shape('hexagonColumn')
+                .size('confirm_add', function (level) {
+                    return [6, 6, level * 10];
+                })
+                .active(true)
+                .color('red')
+                .style({
+                    opacity: 1.0,
+                    strokeWidth: 50,
+                    offsets: [100, 100]
+                });
+
+            pointLayer1.on('mousemove', (e) => {
+                const popup = new Popup({
+                    offsets: [10, 10],
+                    closeButton: false,
+                })
+                    .setLnglat({ lng: e.feature.league_lng, lat: e.feature.league_lat })
+                    .setHTML(`<span>今日确诊: ${e.feature.confirm_add}</span>`);
+                scene.addPopup(popup);
+            });
+            scene.addLayer(pointLayer1);
+
+               // 添加每日治愈的3d柱状图层
+               const pointLayer2 = new PointLayer({zIndex:999}).source(this.leagueListData_ch, {
+                parser: {
+                    type: 'json',
+                    x: 'league_lng',
+                    y: 'league_lat',
+                },
+            }).shape('squareColumn')
+                .size('confirm_heal', function (level) {
+                    return [6, 6, level * 10];
+                })
+                .active(true)
+                .color('green')
+                .style({
+                    opacity: 1.0,
+                    strokeWidth: 50,
+                    offsets: [100, 300]
+                });
+
+            pointLayer2.on('mousemove', (e) => {
+                const popup = new Popup({
+                    offsets: [10, 10],
+                    closeButton: false,
+                })
+                    .setLnglat({ lng: e.feature.league_lng, lat: e.feature.league_lat })
+                    .setHTML(`<span>今日治愈: ${e.feature.confirm_heal}</span>`);
+                scene.addPopup(popup);
+            });
+            scene.addLayer(pointLayer2);
+
+            // 添加放大缩小
+            const zoomControl = new Zoom({
+                position: 'bottomright',
+            });
+            // 添加比例尺
+            const scaleControl = new Scale({
+                position: 'bottomright',
+            });
+
+            scene.addControl(scaleControl);
+            scene.addControl(zoomControl);
             scene.render();
         });
 
@@ -125,9 +216,6 @@ export default class SoccerEpidemic extends Vue {
                 <div id="soccer-container" ></div>
                 <div class="display-table">
                     <Table></Table>
-                </div>
-                <div class="display-trend">
-                    <Trend></Trend>
                 </div>
             </div>
         );
